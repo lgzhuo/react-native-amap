cbeeda105088863cbfb4cc2f7a34c4b66a05721f
lgzhuo <lgzhuo@126.com>
2018-06-15 16:50:45 +0800

    修复ios下聚合点clean的bug

--- lib/ios/RCTAMap/CoordinateQuadTree.m
+++ lib/ios/RCTAMap/CoordinateQuadTree.m
@@ -18,13 +18,13 @@ BoundingBox BoundingBoxForMapRect(MAMapRect mapRect)
 {
     CLLocationCoordinate2D topLeft = MACoordinateForMapPoint(mapRect.origin);
     CLLocationCoordinate2D botRight = MACoordinateForMapPoint(MAMapPointMake(MAMapRectGetMaxX(mapRect), MAMapRectGetMaxY(mapRect)));
-    
+
     CLLocationDegrees minLat = botRight.latitude;
     CLLocationDegrees maxLat = topLeft.latitude;
-    
+
     CLLocationDegrees minLon = topLeft.longitude;
     CLLocationDegrees maxLon = botRight.longitude;
-    
+
     return BoundingBoxMake(minLat, minLon, maxLat, maxLon);
 }
 
@@ -32,35 +32,35 @@ BoundingBox quadTreeNodeDataArrayForPOIs(QuadTreeNodeData *dataArray, NSArray<Cl
 {
     CLLocationDegrees minX = (pois[0]).latitude;
     CLLocationDegrees maxX = (pois[0]).latitude;
-    
+
     CLLocationDegrees minY = (pois[0]).longitude;
     CLLocationDegrees maxY = (pois[0]).longitude;
-    
+
     for (NSInteger i = 0; i < [pois count]; i++)
     {
         dataArray[i] = QuadTreeNodeDataForPoint(pois[i]);
-        
+
         if (dataArray[i].x < minX)
         {
             minX = dataArray[i].x;
         }
-        
+
         if (dataArray[i].x > maxX)
         {
             maxX = dataArray[i].x;
         }
-        
+
         if (dataArray[i].y < minY)
         {
             minY = dataArray[i].y;
         }
-        
+
         if (dataArray[i].y > maxY)
         {
             maxY = dataArray[i].y;
         }
     }
-    
+
     return BoundingBoxMake(minX, minY, maxX, maxY);
 }
 
@@ -69,25 +69,25 @@ BoundingBox quadTreeNodeDataArrayForPOIs(QuadTreeNodeData *dataArray, NSArray<Cl
 - (NSArray<Cluster*> *)clusteredWithinMapRect:(MAMapRect)rect zoomScale:(double)zoomScale size:(float)size
 {
     double scaleFactor = zoomScale / size;
-    
+
     NSInteger minX = floor(MAMapRectGetMinX(rect) * scaleFactor);
     NSInteger maxX = floor(MAMapRectGetMaxX(rect) * scaleFactor);
     NSInteger minY = floor(MAMapRectGetMinY(rect) * scaleFactor);
     NSInteger maxY = floor(MAMapRectGetMaxY(rect) * scaleFactor);
-    
+
     NSMutableArray *clustered = [[NSMutableArray alloc] init];
     for (NSInteger x = minX; x <= maxX; x++)
     {
         for (NSInteger y = minY; y <= maxY; y++)
         {
             MAMapRect mapRect = MAMapRectMake(x / scaleFactor, y / scaleFactor, 1.0 / scaleFactor, 1.0 / scaleFactor);
-            
+
             __block double totalX = 0;
             __block double totalY = 0;
             __block int     count = 0;
-            
+
             NSMutableArray *points = [[NSMutableArray alloc] init];
-            
+
             /* 查询区域内数据的个数. */
             QuadTreeGatherDataInRange(self.root, BoundingBoxForMapRect(mapRect), ^(QuadTreeNodeData data)
                                       {
@@ -96,7 +96,7 @@ BoundingBox quadTreeNodeDataArrayForPOIs(QuadTreeNodeData *dataArray, NSArray<Cl
                                           count++;
                                           [points addObject:(__bridge ClusterPoint *)data.data];
                                       });
-            
+
             if (count >= 1)
             {
                 CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(totalX / count, totalY / count);
@@ -104,22 +104,22 @@ BoundingBox quadTreeNodeDataArrayForPOIs(QuadTreeNodeData *dataArray, NSArray<Cl
             }
         }
     }
-    
+
     return [NSArray arrayWithArray:clustered];
 }
 
 -(void)buildTreeWithPoints:(NSArray<ClusterPoint *> *)pois{
     QuadTreeNodeData *dataArray = malloc(sizeof(QuadTreeNodeData) * [pois count]);
-    
+
     BoundingBox maxBounding = quadTreeNodeDataArrayForPOIs(dataArray, pois);
-    
+
     /*若已有四叉树，清空.*/
     [self clean];
-    
+
     NSLog(@"build tree.");
     /*建立四叉树索引. */
     self.root = QuadTreeBuildWithData(dataArray, [pois count], maxBounding, 4);
-    
+
     free(dataArray);
 }
 
@@ -129,8 +129,9 @@ BoundingBox quadTreeNodeDataArrayForPOIs(QuadTreeNodeData *dataArray, NSArray<Cl
     {
         NSLog(@"free tree.");
         FreeQuadTreeNode(self.root);
+        self.root = nil;
     }
-    
+
 }
 
 @end
